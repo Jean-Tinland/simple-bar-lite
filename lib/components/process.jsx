44ce58863ba099add410e8abd2affbea9d9b30b9
Jean Tinland <jean.tinland@gmail.com>
2022-01-07 11:43:21 +0100

    Hide app name if contained in app title in process

--- lib/components/process.jsx
+++ lib/components/process.jsx
@@ -1,13 +1,19 @@
 import * as Uebersicht from 'uebersicht'
 import * as Slider from '../services/slider'
 
+const getName = (app, title) => {
+  if (!title.length || app === title) return app
+  if (title.includes(app)) return title
+  return `${app} | ${title}`
+}
+
 const Process = ({ currentWindow }) => {
   const ref = Uebersicht.React.useRef()
 
   if (!currentWindow) return null
 
   const { app, title } = currentWindow
-  const processName = app !== title && title.length ? `${app} | ${title}` : app
+  const processName = getName(app, title)
 
   const onMouseEnter = () => Slider.start(ref.current, '.spl-process__inner', '.spl-process__name')
   const onMouseLeave = () => Slider.stop(ref.current, '.spl-process__name')
